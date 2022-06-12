628c52fd99e853b686822c98bb7fd48f55382c87
Pear-Juice <53879861+Pear-Juice@users.noreply.github.com>
2022-06-12 15:37:49 -0600

    Changes and Deletions
    Power cable and removed github integration

--- Scripts/Powerline.gd
+++ Scripts/Powerline.gd
@@ -3,14 +3,16 @@ extends Line2D
 export var initialPoint: Vector2
 export var distanceToDelete: float
 var deleteSecondLastPoint: bool
-var playerPos: Vector2
+
+var attachmentNode: Node2D
+var attachmentPos: Vector2
 
 func _ready():
 	set_as_toplevel(true)
 	add_point(initialPoint)
 
 func _process(delta):
-	playerPos = get_parent().global_position
+	attachmentPos = get_attachment_position()
 	
 	if Input.is_action_just_pressed("add_point"):
 		deleteSecondLastPoint = false
@@ -20,19 +22,24 @@ func _process(delta):
 		points[points.size() - 1] = get_parent().global_position
 		check_point_deletion()
 		
+
+func get_attachment_position() -> Vector2:
+	return attachmentNode.global_position
 	
+func set_attachment_node(node: Node2D):
+	attachmentNode = node
 
 func place_point():
 	add_point(get_parent().global_position)
 	
 func check_point_deletion():
 	if !deleteSecondLastPoint && points[points.size() - 2]:
-		if points[points.size() - 2].distance_to(playerPos) > distanceToDelete:
+		if points[points.size() - 2].distance_to(attachmentPos) > distanceToDelete:
 			deleteSecondLastPoint = true
 	
 	var index: int
 	for point in points:
-		if point.distance_to(playerPos) < distanceToDelete && point != points[points.size() - 1]:
+		if point.distance_to(attachmentPos) < distanceToDelete && point != points[points.size() - 1]:
 			if deleteSecondLastPoint:
 				delete_point_recursive(index)
 			elif point != points[points.size() - 2]:
