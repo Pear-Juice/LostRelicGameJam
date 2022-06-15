5d28d2d7d2d4fe8397418b900df2ff78ff7820fa
Pear-Juice <53879861+Pear-Juice@users.noreply.github.com>
2022-06-15 15:12:45 -0600

    movable box kindof

--- Scripts/Puzzle Pieces/MovableBox.gd
+++ Scripts/Puzzle Pieces/MovableBox.gd
@@ -1,16 +1,16 @@
-extends Node
+extends KinematicBody2D
 
+export (int) var speed
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+var move: bool
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _on_give_power():
+	move = true
+	
+func _on_take_power():
+	move = false
+	
+func _process(delta: float) -> void:
+	if move:
+		move_and_collide(Vector2(0, speed))
+	
