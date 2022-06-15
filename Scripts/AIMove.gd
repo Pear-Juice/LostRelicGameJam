20a5cb29d199cacb706bfc5d3592c40f52dcdc71
hypercatw <78281913+hypercatw@users.noreply.github.com>
2022-06-16 00:06:45 +0300

    enemy:hermit animation system (wip)

--- Scripts/AIMove.gd
+++ Scripts/AIMove.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+export (NodePath) var animatorPath: NodePath
+onready var _animator = get_node(animatorPath) as AnimatedSprite
+
 onready var _kinematicBody = get_parent() as KinematicBody2D
 onready var _player = get_node("/root/PlayerVariables").node as KinematicBody2D
 
-
+var velocity = Vector2()
 export (int) var speed = 100
 
 # Called when the node enters the scene tree for the first time.
@@ -11,8 +14,31 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(_delta):
-	var direction = (_player.global_position - global_position).normalized()
-	_kinematicBody.move_and_slide(direction * speed)
+	velocity = (_player.global_position - global_position).normalized()
+	_kinematicBody.move_and_slide(velocity * speed)
+	animate()
+
+	# animation
+func animate():
+	print(velocity)
+	var a_table = [["walk-l", "idle-l"], ["walk-r", "idle-r"], ["walk-u", "idle-u"], ["walk-d", "idle-d"]]
+	if (velocity == Vector2()):
+		for a in a_table:
+			if (_animator.animation == a[0]):
+				_animator.animation = a[1]
+				
+	if (velocity.x != 0 && velocity.y != 0):
+		return
+	
+	elif (velocity.y < 0):
+		_animator.animation = "walk-u"
+	elif (velocity.y > 0):
+		_animator.animation = "walk-d"
+	elif (velocity.x > 0):
+		_animator.animation = "walk-r"
+	elif (velocity.x < 0):
+		_animator.animation = "walk-l"
+
 
 func get_nav_path():
 	pass
