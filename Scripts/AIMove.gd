9d8afadf0a1fb9cc8fa79807af91655443017915
hypercatw <78281913+hypercatw@users.noreply.github.com>
2022-06-15 21:44:37 +0300

    add enemy:hermit prefab with basic AI (no pathfinding)

---
+++ Scripts/AIMove.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+onready var _kinematicBody = get_parent() as KinematicBody2D
+onready var _player = get_node("/root/PlayerVariables").node as KinematicBody2D
+
+
+export (int) var speed = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(_delta):
+	var direction = (_player.global_position - global_position).normalized()
+	_kinematicBody.move_and_slide(direction * speed)
+	print_debug(direction)
+
+func get_nav_path():
+	pass
