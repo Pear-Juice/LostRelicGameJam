af0b3ebca4e9bc7fe8eb8f18d0fb4685ea9d58d4
Pear-Juice <53879861+Pear-Juice@users.noreply.github.com>
2022-06-16 15:16:34 -0600

    Scene switching almost

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,13 +6,23 @@ onready var motor = node.get_node("Movement") as Node
 
 #null if you arent holding
 var powerline: Line2D
-export var spawnPoint: Vector2
 
 export (int) var baseHealth = 4
 var health = 4
 
+export var currentSpawnPoint : String
+var spawnpoints : Array
+var spawnpointNames : Array
+
+func _ready() -> void:
+	node = get_tree().get_nodes_in_group("Player")[0] as KinematicBody2D
+	
+	if (self == node):
+		node.spawn(currentSpawnPoint)
+
 func _process(_delta: float) -> void:
-	node.powerline = powerline
+	if node && powerline:
+		node.powerline = powerline
 
 func receive_damage(dmg):
 	node.health -= dmg
@@ -23,12 +33,21 @@ func die():
 	animationPlayer.play("Death 1")
 	
 	if powerline:
-		node.powerline.stop_generation()
+		powerline.stop_generation()
 	
 	yield(get_tree().create_timer(0.5), "timeout")
-	node.global_position = node.spawnPoint
-	
-	node.health = node.baseHealth
+	node.spawn(currentSpawnPoint)
+	health = baseHealth
 
 func add_force(force: Vector2):
 	motor.ext_velocity = force
+	
+func spawn(pointName: String):
+	var pointPos : Vector2
+	
+	for i in range(0, spawnpoints.size()):
+		if (spawnpointNames[i] == pointName):
+			pointPos = spawnpoints[i]
+			break
+	
+	node.global_position = pointPos
--- Scripts/SceneSwitcher.gd
+++ Scripts/SceneSwitcher.gd
@@ -1,7 +1,11 @@
-extends Node
+extends Area2D
 
 export var scene : PackedScene
+export var spawnPoint : String
 onready var player = get_node("/root/PlayerVariables")
+
+func _ready() -> void:
+	connect("body_entered", self, "switch_scene")
 
 func switch_scene(body):
 	if body == player.node:
