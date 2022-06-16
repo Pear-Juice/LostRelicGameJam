239126f4b1ba601c838b24462db8650497f9e885
Pear-Juice <53879861+Pear-Juice@users.noreply.github.com>
2022-06-15 18:32:30 -0600

    movable blocks kill you

--- Scripts/Puzzle Pieces/MovableBox.gd
+++ Scripts/Puzzle Pieces/MovableBox.gd
@@ -3,6 +3,7 @@ tool
 extends KinematicBody2D
 
 onready var connector = get_node("Cable Connector") as Node2D
+onready var player = get_node("/root/PlayerVariables")
 
 export (int) var speed
 enum ConnectorDirection{Front, Back, Left, Right}
@@ -38,6 +39,16 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	if not Engine.editor_hint:
 		if move:
-			move_and_collide(Vector2(0,speed).rotated(rotation))
+			var collision = move_and_collide(Vector2(0,speed).rotated(rotation))
+			if collision:
+				if collision.collider.is_class("KinematicBody2D"):
+					collision.collider.move_and_collide(Vector2(0,speed).rotated(rotation))
+					if collision.collider.get_script() == player.node.get_script() && check_stuck(collision.collider):
+						player.node.die()
+					
+func check_stuck(node: KinematicBody2D) -> bool:
+	var pos = node.global_position
+	if node.test_move(node.transform, Vector2(0,0)):
+		return true
 		
-	
+	return false
