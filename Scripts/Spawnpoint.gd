a49a224b95807a1feae7e105ee66725f78de73a1
Pear-Juice <53879861+Pear-Juice@users.noreply.github.com>
2022-06-16 20:13:06 -0600

    tests

--- Scripts/Spawnpoint.gd
+++ Scripts/Spawnpoint.gd
@@ -6,8 +6,10 @@ export var pointName : String
 # Called when the node enters the scene tree for the first time.
 func initialize():
 	print("init")
-	Spawn.spawnpointNames.append(pointName)
-	Spawn.spawnpoints.append(global_position)
+	
+	if (!Spawn.spawnpointNames.has(pointName) && !Spawn.spawnpoints.has(global_position)):
+		Spawn.spawnpointNames.append(pointName)
+		Spawn.spawnpoints.append(global_position)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
