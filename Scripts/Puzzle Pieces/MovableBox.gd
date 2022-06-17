ae7525e3818ca785964f4ba3609ac59f6a117295
Pear-Juice <53879861+Pear-Juice@users.noreply.github.com>
2022-06-17 15:16:59 -0600

    Fixed boxes

--- Scripts/Puzzle Pieces/MovableBox.gd
+++ Scripts/Puzzle Pieces/MovableBox.gd
@@ -46,16 +46,45 @@ func _process(delta: float) -> void:
 		if connectorDirection == ConnectorDirection.Front:
 			pass
 
-	
+func collide_move(dir: Vector2):
+	move_and_collide(dir)
+
+var isMoving: bool
 func _physics_process(delta: float) -> void:
 	if not Engine.editor_hint:
+		var dir = Vector2(0,0)
 		if move:
-			var collision = move_and_collide(Vector2(0,speed).rotated(rotation))
-			if collision:
-				if collision.collider.is_class("KinematicBody2D"):
-					collision.collider.move_and_collide(Vector2(0,speed).rotated(rotation))
-					if collision.collider.get_script() == player.node.get_script() && check_stuck(collision.collider):
-						player.node.die()
+			dir = Vector2(0,speed).rotated(rotation)
+			isMoving = true
+		else:
+			if (isMoving):
+				dir = -Vector2(0,speed).rotated(rotation)
+				isMoving = false
+			else:
+				dir = Vector2(0,0)
+				
+		
+		var collision = move_and_collide(dir)
+		
+		if collision && collision.collider.is_class("KinematicBody2D"):
+			#if is player
+			if collision.collider.get_script() == player.node.get_script():
+				player.node.move_and_collide(dir)
+				if check_stuck(collision.collider):
+					player.node.die()
+			if collision.collider.get_script() == self.get_script():
+				collision.collider.move_and_collide(dir)
+			
+		
+		
+		
+
+		
+		#if collision:
+		#	if collision.collider.is_class("KinematicBody2D"):
+		#		collision.collider.move_and_collide(Vector2(0,speed).rotated(rotation))
+		#		if collision.collider.get_script() == player.node.get_script() && check_stuck(collision.collider):
+		#			player.node.die()
 					
 func check_stuck(node: KinematicBody2D) -> bool:
 	var pos = node.global_position
